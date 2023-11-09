98ee917e8712ff7e3e3f370ec324c69471679fd9
Yi-Chia Chang <yichia3@illinois.edu>
2023-11-09 16:13:24 -0600

    process tif in smaller chunck

--- create_boundary_china.py
+++ create_boundary_china.py
@@ -1,41 +1,35 @@
 import rasterio
 from rasterio.features import shapes
+from shapely.geometry import shape, Polygon
+from shapely.ops import unary_union
 import geopandas as gpd
-from shapely.geometry import shape, Polygon, mapping
 import fiona
-from fiona.crs import from_epsg
 
 # Function to create a shapefile illustrating the boundary of a TIFF file, ignoring the no-data class
 def create_boundary_shapefile(tiff_path, shapefile_path, no_data_value):
+    boundary = Polygon()
     # Open the TIFF file
     with rasterio.open(tiff_path) as src:
-        # Read the raster data, masking out the no-data values
-        image = src.read(1)
-        mask = image != no_data_value
+        # Read the raster data in windows
+        for ji, window in src.block_windows(1):
+            # Read the data in the current window
+            image = src.read(1, window=window)
+            mask = image != no_data_value
+            if mask.sum() > 0:  # Only proceed if there is data in the window
+                # Extract shapes from the masked data
+                for geom, value in shapes(image, mask=mask, transform=src.window_transform(window)):
+                    if value != no_data_value:
+                        # Convert the geometry to a shapely shape and union it with the boundary
+                        boundary = unary_union([boundary, shape(geom)])
 
-        # Extract shapes from the masked data
-        results = ({'properties': {'raster_val': v}, 'geometry': s}
-                   for i, (s, v) in enumerate(shapes(image, mask=mask, transform=src.transform)))
+    # Create a GeoDataFrame
+    gdf = gpd.GeoDataFrame([1], crs=src.crs, geometry=[boundary])
 
-        # Filter out the geometries that correspond to no-data values
-        geoms = [shape(geom['geometry']) for geom in results if geom['properties']['raster_val'] != no_data_value]
+    # Save the GeoDataFrame to a shapefile
+    gdf.to_file(shapefile_path, driver='ESRI Shapefile')
 
-        # Combine the geometries to get the boundary of all data-containing areas
-        boundary = Polygon()
-        for geom in geoms:
-            if isinstance(geom, Polygon):
-                boundary = boundary.union(geom)
-            else:
-                for polygon in geom:
-                    boundary = boundary.union(Polygon(polygon))
+    print(f"Shapefile created at {shapefile_path}")
 
-        # Create a GeoDataFrame
-        gdf = gpd.GeoDataFrame([1], crs=src.crs, geometry=[boundary])
-
-        # Save the GeoDataFrame to a shapefile
-        gdf.to_file(shapefile_path, driver='ESRI Shapefile')
-
-        print(f"Shapefile created at {shapefile_path}")
 
 # Path to your TIFF file
 tiff_file_path = '/projects/dali/data/china/CDL2019_clip.tif'
