e48fc1cd70b3df41a61ad2fc05d961cfed97b0f4
Yi-Chia Chang <yichia3@illinois.edu>
2023-11-11 19:15:17 -0600

    sample points visualization

---
+++ plot_coordinates.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+# Load the CSV file
+df = pd.read_csv('data/sampled_locations.csv', header=None, names=['idx', 'x', 'y'])
+gdf = gpd.read_file('data/northeast/ChinaAgriREZone.shp')
+
+if gdf.crs.to_string() != 'EPSG:4326':
+    gdf = gdf.to_crs(epsg=4326)
+
+
+# Create a figure with an appropriate size
+plt.figure(figsize=(10, 5))
+
+# Set up a GeoAxes with the PlateCarree projection
+# This projection is a simple and commonly used map projection
+ax = plt.axes(projection=ccrs.PlateCarree())
+
+# Add coastlines for context
+ax.coastlines()
+
+# Plot the points
+plt.scatter(df['x'], df['y'], color='blue', marker='o', transform=ccrs.Geodetic())
+gdf.plot(ax=ax, facecolor='none', edgecolor='red', linewidth=2)
+
+# gdf.plot(ax=ax, facecolor='none', edgecolor='red', linewidth=2, transform=ccrs.Geodetic())
+
+# Add gridlines and labels for readability
+ax.gridlines(draw_labels=True)
+
+# Show the plot
+plt.show()
