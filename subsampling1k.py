26b91ef6996a4df13b74937369e8ee5ff46d268c
Yi-Chia Chang <yichia3@illinois.edu>
2024-06-02 23:21:33 -0400

    cdl 10

--- subsampling1k.py
+++ subsampling1k.py
@@ -25,7 +25,7 @@ def copy_random_files(src_folder, dest_folder, num_files, log_file):
             f.write(file_name + '\n')
 
 # Parameters
-num_files = 100
+num_files = 10
 src_folder = '/data/yichiac/sentinel2_subsample/sentinel2_cdl_2023_subsampled'
 dest_folder = '/data/yichiac/sentinel2_subsample_'+str(num_files)+'/sentinel2_cdl_2023_subsampled'
 log_file = '/data/yichiac/sample_files/cdl_subsampled_'+str(num_files)+'.txt'
