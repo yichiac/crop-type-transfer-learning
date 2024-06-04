5b1f05278491425811dc5ca6f92743876e805256
Yi-Chia Chang <yichia3@illinois.edu>
2024-06-04 13:24:37 -0400

    fix sample

--- create_test_samples.py
+++ create_test_samples.py
@@ -1,8 +1,8 @@
 import os
-# import random
+import random
 import shutil
 
-def copy_random_files(src_folder, all_folder, dest_folder, log_file):
+def copy_random_files(src_folder, all_folder, dest_folder, log_file, num_files):
     # Create the destination folder if it doesn't exist
     if not os.path.exists(dest_folder):
         os.makedirs(dest_folder)
@@ -11,9 +11,10 @@ def copy_random_files(src_folder, all_folder, dest_folder, log_file):
     train_files = [f for f in os.listdir(src_folder) if f.endswith('.tif')]
     all_files = [f for f in os.listdir(all_folder) if f.endswith('.tif')]
     selected_files = list(set(all_files) - set(train_files))
-    print('num of train files: ', len(train_files))
-    print('num of all files: ', len(all_files))
-    print('num of different files: ', len(selected_files))
+    if num_files > len(selected_files):
+        print('warning: number of files is larger than selected files')
+        print(len(selected_files))
+    selected_files = random.sample(selected_files, min(num_files, len(selected_files)))
 
     # Copy each selected file to the destination folder
     for file_name in selected_files:
@@ -27,11 +28,11 @@ def copy_random_files(src_folder, all_folder, dest_folder, log_file):
             f.write(file_name + '\n')
 
 # Parameters
-num_files = 900
+num_files = 990
 src_folder = '/data/yichiac/sentinel2_subsample_cdl_ood_'+str(1000-num_files)+'/sentinel2_cdl_2023_subsampled'
 all_folder = '/data/yichiac/sentinel2_subsample_1000/sentinel2_cdl_2023_subsampled'
 dest_folder = '/data/yichiac/sentinel2_subsample_cdl_ood_'+str(num_files)
 log_file = '/data/yichiac/sample_files_test/cdl_subsampled_'+str(num_files)+'.txt'
 
 # Call the function
-copy_random_files(src_folder, all_folder, dest_folder, log_file)
+copy_random_files(src_folder, all_folder, dest_folder, log_file, num_files)
