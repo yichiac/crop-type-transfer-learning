8a467c97fba67030c32f0851835986ce509d1f58
Yi-Chia Chang <yichia3@illinois.edu>
2024-06-04 11:52:19 -0400

    how many selected files

--- create_test_samples.py
+++ create_test_samples.py
@@ -11,6 +11,7 @@ def copy_random_files(src_folder, all_folder, dest_folder, log_file):
     train_files = [f for f in os.listdir(src_folder) if f.endswith('.tif')]
     all_files = [f for f in os.listdir(all_folder) if f.endswith('.tif')]
     selected_files = list(set(all_files) - set(train_files))
+    print('num of different files: ', len(selected_files))
 
     # Copy each selected file to the destination folder
     for file_name in selected_files:
@@ -27,7 +28,7 @@ def copy_random_files(src_folder, all_folder, dest_folder, log_file):
 num_files = 900
 src_folder = '/data/yichiac/sentinel2_subsample_cdl_ood_'+str(1000-num_files)+'/sentinel2_cdl_2023_subsampled'
 all_folder = '/data/yichiac/sentinel2_subsample_1000/sentinel2_cdl_2023_subsampled'
-dest_folder = '/data/yichiac/sentinel2_subsample_'+str(num_files)
+dest_folder = '/data/yichiac/sentinel2_subsample_cdl_'+str(num_files)
 log_file = '/data/yichiac/sample_files_test/cdl_subsampled_'+str(num_files)+'.txt'
 
 # Call the function
