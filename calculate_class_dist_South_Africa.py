33c7a508b96c308f3cec4917c888c13e5d4d8ddd
Yi-Chia Chang <yichia3@illinois.edu>
2024-06-09 13:49:55 -0400

    update sact class count

--- calculate_class_dist_South_Africa.py
+++ calculate_class_dist_South_Africa.py
@@ -3,7 +3,8 @@ import rasterio
 import numpy as np
 
 # Define the directory where to start searching for raster files
-root_directory = '/projects/dali/data/south_africa_crop_type/train/labels'
+# root_directory = '/projects/dali/data/south_africa_crop_type/train/labels'
+root_directory = '/data/yichiac/sact_harmonized/train/labels'
 
 # This dictionary will hold the aggregated class distribution
 aggregated_class_distribution = {}
@@ -12,7 +13,8 @@ aggregated_class_distribution = {}
 for subdir, dirs, files in os.walk(root_directory):
     for file in files:
         # Check if the file is label not field_ids
-        if not file.endswith('field_ids.tif'):
+        # if not file.endswith('field_ids.tif'):
+        if file.endswith('.tif'):
             # Construct the full file path
             file_path = os.path.join(subdir, file)
 
