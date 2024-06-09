0c6cefb04e58f1f9d22459b02f916c30fe112dca
Yi-Chia Chang <yichia3@illinois.edu>
2024-06-09 15:26:43 -0400

    update sact and agrifieldnet

--- calculate_class_dist_India.py
+++ calculate_class_dist_India.py
@@ -3,25 +3,19 @@ import rasterio
 import numpy as np
 
 # Define the directory where to start searching for raster files
-# root_directory = '/projects/dali/data/agrifieldnet/ref_agrifieldnet_competition_v1_labels_train'
+img_directory = '/data/yichiac/sentinel2_subsample_1000/sentinel2_agrifieldnet_2021_subsampled'
 root_directory = '/data/yichiac/agrifieldnet_harmonized/train_labels'
-
-# This dictionary will hold the aggregated class distribution
 aggregated_class_distribution = {}
 
 # Walk through all subdirectories in the root directory
-for subdir, dirs, files in os.walk(root_directory):
+for subdir, dirs, files in os.walk(img_directory):
     for file in files:
-        # Check if the file is 'raster_labels.tif'
-        # if file == 'raster_labels.tif':
         if file.endswith('.tif'):
-            # Construct the full file path
-            file_path = os.path.join(subdir, file)
+            file_path = os.path.join(root_directory, file[5:])
 
             # Read the raster file
             with rasterio.open(file_path) as src:
-                # Read the data as a one-dimensional array
-                data = src.read(1).flatten()  # Flatten the data
+                data = src.read(1).flatten()
 
                 # Compute the histogram
                 unique, counts = np.unique(data, return_counts=True)
@@ -33,8 +27,6 @@ for subdir, dirs, files in os.walk(root_directory):
                     else:
                         aggregated_class_distribution[class_value] = count
 
-# Now aggregated_class_distribution contains the total class counts across all raster_labels.tif files
-# Print the aggregated distributions:
 print(f"Aggregated Class Distribution:")
 for class_value, count in aggregated_class_distribution.items():
     print(f"Class {class_value}: {count}")
