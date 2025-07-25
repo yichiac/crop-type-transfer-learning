77ebf3e2d619ae2b4603a23f3a20943d5f465447
Yi-Chia Chang <yichia3@illinois.edu>
2025-07-25 15:25:39 -0400

    update readme, create train/test splits

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 [![Hugging Face](https://img.shields.io/badge/HuggingFace-TorchGeo-yellow?logo=huggingface&logoColor=yellow)](https://huggingface.co/torchgeo)
 [![GitHub](https://img.shields.io/badge/GitHub-TorchGeo-4CB05B?logo=github&logoColor=white)](https://github.com/microsoft/torchgeo)
 
-This is the official repository for the paper "_On the Generalizability of Foundation Models for Crop Type Mapping_".
-<!-- Transfer Learning for Global Crop Type Mapping with Foundation Models -->
+This is the official repository for the paper "_On the Generalizability of Foundation Models for Crop Type Mapping_", acctped at [IEEE IGARSS 2025](https://www.2025.ieeeigarss.org/).
+
+Authors: [Yi-Chia Chang](https://yichiac.github.io/), [Adam J. Stewart](https://github.com/adamjstewart), [Favyen Bastani](https://favyen.com/), [Piper Wolters](https://piperwolters.com/), Shreya Kannan, George R. Huber, Jingtong Wang, [Arindam Banerjee](https://arindam.cs.illinois.edu/index.html).
+
 
 ## Installation
 
@@ -45,7 +47,11 @@ Sentinel-2 donwload pipeline can be found in [Ai2 rslearn_projects](https://gith
 
 
 ### Data Split
-Follow the files in `train-test-splits/` to split the dataset into training and testing sets.
+Follow the files in `train-test-splits/` to split the dataset into training and testing sets. For demo, you can run the following command to create train/test data for a CDL OOD experiment:
+```sh
+python3 train-test-splits/create_splits.py
+```
+
 
 ### Training
 Run the training script for CDL with SSL4EO-S12 pre-trained weights:
--- train-test-splits/create_splits.py
+++ train-test-splits/create_splits.py
@@ -2,24 +2,53 @@ import os
 import shutil
 import csv
 
-datasets = ['cdl', 'eurocrops', 'nccm', 'sact', 'sas']
-sizes = [10, 100, 900]
+regions = ['cdl_2023', 'eurocrops', 'nccm_2019', 'sact_2017', 'sas_2021']
+train_region = 'cdl_2023'
+size = 100 # sizes = [10, 100, 900]
 
-csv_path = './train-test-splits/sentinel2_cdl_2023_subsampled_test.csv'
-source_dir = './harmonized_global_crops/sentinel2_subsample_1000/sentinel2_cdl_2023_subsampled/'
-destination_dir = './harmonized_global_crops/sentinel2_subsample_test/'
+def copy_files(csv_path, source_dir, destination_dir):
+    """Copy files listed in a CSV to a destination directory."""
+    with open(csv_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row:
+                filename = row[0].strip()
+                src_file = os.path.join(source_dir, filename)
+                dst_file = os.path.join(destination_dir, filename)
+                if os.path.exists(src_file):
+                    shutil.copy(src_file, dst_file)
+                else:
+                    print(f'File not found: {filename}')
+    print('Completed copying files.')
 
-os.makedirs(destination_dir, exist_ok=True)
+# create test split
+for region in regions:
+    print(f'Creating test split for {region}')
+    csv_path = f'./train-test-splits/sentinel2_{region}_subsampled_test.csv'
+    source_dir = f'./harmonized_global_crops/sentinel2_subsample/sentinel2_{region}_subsampled/'
+    destination_dir = f'./harmonized_global_crops/sentinel2_subsample_test/sentinel2_{region}_subsampled/'
+    os.makedirs(destination_dir, exist_ok=True)
+    copy_files(csv_path, source_dir, destination_dir)
 
-with open(csv_path, newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        if row:
-            filename = row[0].strip()
-            src_file = os.path.join(source_dir, filename)
-            dst_file = os.path.join(destination_dir, filename)
-            if os.path.exists(src_file):
-                shutil.copy(src_file, dst_file)
-                print(f'Copied: {filename}')
-            else:
-                print(f'File not found: {filename}')
+# create train split
+print(f'Creating train split for {train_region} with size {size}')
+csv_path = f'./train-test-splits/sentinel2_{train_region}_subsampled_{size}.csv'
+source_dir = './harmonized_global_crops/sentinel2_subsample'
+destination_dir = f'./harmonized_global_crops/sentinel2_subsample_{train_region}_{size}_ood/'
+target_source_dir = os.path.join(source_dir, f'sentinel2_{train_region}_subsampled')
+target_destination_dir = os.path.join(destination_dir, f'sentinel2_{train_region}_subsampled')
+os.makedirs(target_destination_dir, exist_ok=True)
+copy_files(csv_path, target_source_dir, target_destination_dir) # copy the selected files in csv
+
+# copy files from other regions
+regions.remove(train_region)
+for region in regions:
+    dataset = f'sentinel2_{region}_subsampled'
+    target_source_dir = os.path.join(source_dir, dataset)
+    target_destination_dir = os.path.join(destination_dir, dataset)
+    if os.path.exists(target_source_dir):
+        shutil.copytree(target_source_dir, target_destination_dir)
+    else:
+        print(f'Source directory does not exist: {target_source_dir}')
+        continue
+    print('Completed copying ', region)
